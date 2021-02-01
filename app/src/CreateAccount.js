8ea19870e0b9a70962e19f79cf7c510fa89f64d2
NathanJWr <nathanielwright@protonmail.com>
2021-02-01 09:15:01 -0800

    Username and password now manage their own state

--- app/src/CreateAccount.js
+++ app/src/CreateAccount.js
@@ -1,54 +1,79 @@
 import React from 'react';
 import { TextInput, View, StyleSheet, Button } from 'react-native';
 
-const UsernameInput = () => {
-    const [value, onChangeText] = React.useState('');
-    const grayRGB = 'rgb(211, 211, 211)';
-    return (
-        <TextInput
-            style={styles.input}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-            placeholder={'Username'}
-            placeholderTextColor={grayRGB}
-            textContentType={'emailAddress'}
-        />
-    );
+const grayRGB = 'rgb(211, 211, 211)';
+class UsernameInput extends React.Component {
+    state = {
+        email: ''
+    };
+
+    onChangeEmail = (val) => {
+        this.setState({['email']: val});
+    }
+
+    render () {
+        return (
+            <TextInput
+                style={styles.input}
+                onChangeText={text => this.onChangeEmail(text)}
+                placeholder={'Username'}
+                placeholderTextColor={grayRGB}
+                textContentType={'emailAddress'}
+            />
+        );
+    }
 }
 
-const PasswordInput = () => {
-    const [value, onChangeText] = React.useState('');
-    const grayRGB = 'rgb(211, 211, 211)';
-    return (
-        <TextInput
-            style={styles.input}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-            placeholder={'Password'}
-            placeholderTextColor={grayRGB}
-            textContentType={'newPassword'}
-            secureTextEntry={true}
-        />
-    );
+class PasswordInput extends React.Component {
+    state = {
+        password: ''
+    };
+
+    onChangePassword = (val) => {
+        this.setState({['password']: val});
+    }
+
+    render() {
+        return (
+            <TextInput
+                style={styles.input}
+                onChangeText={val => this.onChangePassword(val)}
+                placeholder={'Password'}
+                placeholderTextColor={grayRGB}
+                textContentType={'newPassword'}
+                secureTextEntry={true}
+            />
+        );
+    }
 }
 
-const SubmitButton = () => {
-    return (
-        <Button
-            title="Sign Up"
-            color='#841584'
-        />
-    );
-}
-
-export const CreateAccount = (props) => { 
-    return (
-        <View style={styles.container}>
-            <UsernameInput/>
-            <PasswordInput/>
-            <SubmitButton/>
-        </View>
-    );
+export class CreateAccount extends React.Component {
+    signUp = async () => {
+        const currentUsernameInput = this.UsernameInput.current;
+        const currentPasswordInput = this.PasswordInput.current;
+        console.log('User sign-up request with ' +
+                    currentUsernameInput.state.email + ' and ' +
+                    currentPasswordInput.state.password);
+    }
+    
+    constructor(props) {
+        super(props);
+        this.UsernameInput = React.createRef();
+        this.PasswordInput = React.createRef();
+    }
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                <UsernameInput ref={this.UsernameInput}/>
+                <PasswordInput ref={this.PasswordInput}/>
+                <Button
+                    title='Sign Up'
+                    onPress={this.signUp}
+                />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
