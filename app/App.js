64b8b72009896a7d1cb4637e2e71153b85efae94
Troi-Ryan S <troiryans@gmail.com>
2021-02-03 19:08:49 -0800

    start firebase api integration

--- app/App.js
+++ app/App.js
@@ -1,4 +1,6 @@
+import * as firebase from 'firebase';
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from './src/components/HomeScreen';
@@ -9,6 +11,22 @@ import { Cards } from './src/components/Cards';
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  // const firebaseConfig = {
+  //   apiKey: 'api-key',
+  //   authDomain: 'project-id.firebaseapp.com',
+  //   databaseURL: 'https://project-id.firebaseio.com',
+  //   projectId: 'project-id',
+  //   storageBucket: 'project-id.appspot.com',
+  //   messagingSenderId: 'sender-id',
+  //   appId: 'app-id',
+  //   measurementId: 'G-measurement-id',
+  // };
+  
+  // firebase.initializeApp(firebaseConfig);
+
+  console.log(process.env.API_KEY);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
