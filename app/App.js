3905471030aec91e1068851557af5354b818717e
Troi-Ryan S <troiryans@gmail.com>
2021-02-03 19:28:57 -0800

    allow inline .env

--- app/App.js
+++ app/App.js
@@ -12,20 +12,18 @@ const Stack = createStackNavigator();
 
 export default function App() {
 
-  // const firebaseConfig = {
-  //   apiKey: 'api-key',
-  //   authDomain: 'project-id.firebaseapp.com',
-  //   databaseURL: 'https://project-id.firebaseio.com',
-  //   projectId: 'project-id',
-  //   storageBucket: 'project-id.appspot.com',
-  //   messagingSenderId: 'sender-id',
-  //   appId: 'app-id',
-  //   measurementId: 'G-measurement-id',
-  // };
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  };
   
-  // firebase.initializeApp(firebaseConfig);
-
-  console.log(process.env.API_KEY);
+  firebase.initializeApp(firebaseConfig);
 
   return (
     <NavigationContainer>
