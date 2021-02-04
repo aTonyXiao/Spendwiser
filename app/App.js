f1fe0f24f315e72425f6de7c05a6d54e29d026ca
Troi-Ryan S <troiryans@gmail.com>
2021-02-03 19:32:33 -0800

    firebase api keys

--- app/App.js
+++ app/App.js
@@ -17,10 +17,10 @@ export default function App() {
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    // storageBucket: process.env.STORAGE_BUCKET,
+    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    // appId: process.env.APP_ID,
+    // measurementId: process.env.MEASUREMENT_ID,
   };
   
   firebase.initializeApp(firebaseConfig);
