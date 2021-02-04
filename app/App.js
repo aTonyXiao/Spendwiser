6973e0eccdbd189442c06b2ed789191772a59292
Nathaniel Kong <nskong@ucdavis.edu>
2021-02-04 10:36:00 -0800

    Added ccstack API and integrated with add card page

--- app/App.js
+++ app/App.js
@@ -6,6 +6,7 @@ import { Login } from './src/components/Login';
 import { CreateAccount } from './src/components/CreateAccount';
 import { Settings } from './src/components/Settings';
 import { Cards } from './src/components/Cards';
+import { AddCard } from './src/components/AddCard';
 
 const Stack = createStackNavigator();
 
@@ -13,10 +14,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-        name="Settings"
-        component={Settings}
-        />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -32,6 +29,14 @@ export default function App() {
         <Stack.Screen
           name="Cards"
           component={Cards}
+        />
+        <Stack.Screen
+          name="AddCard"
+          component={AddCard}
+        />
+        <Stack.Screen
+        name="Settings"
+        component={Settings}
         />
       </Stack.Navigator>
     </NavigationContainer>
