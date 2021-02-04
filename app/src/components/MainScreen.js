fa931d07a0e9c23ffe96e27c29a21f4c0717a768
Wei Xuan (Nathan) Ng <wxng@ucdavis.edu>
2021-02-03 23:49:10 -0800

    added geolocation and google places api

---
+++ app/src/components/MainScreen.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Image, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const googlePlaceSearchURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+
+const googleAPIKey = "&radius=100&key=AIzaSyBjvCHsXdtG924_6DpetOLJPliM--FKWWQ"
+export function MainScreen() {
+    const [curLoc, setCurLoc] = useState("UC Davis Memorial Union");
+    const [curAddress, setCurAddress] = useState("250 W Quad, Davis, CA 95616");
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [region, setRegion] = useState({
+        latitude: 38.542530, 
+        longitude: -121.749530,
+        latitudeDelta: 0.0052,
+        longitudeDelta: 0.0051,
+    })
+    const [isLoading, setLoading] = useState(true);
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0052,
+                longitudeDelta: 0.0051,
+            })
+            fetch(googlePlaceSearchURL + location.coords.latitude + "," + location.coords.longitude + googleAPIKey)
+            .then((response) => response.json())
+            .then((json) => setPlaces(json.results))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
+        })();
+        }, []);
+
+    // let text = 'Waiting..';
+    // if (errorMsg) {
+    //     text = errorMsg;
+    // } else if (location) {
+    //     text = JSON.stringify(location);
+    // }
+    
+    // console.log(text);
+    return (
+        <View style={styles.screen}>
+            <View style={styles.container}>
+                <MapView style={styles.map}
+                    provider = "google"
+                    region = {region}
+                >
+                <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+                </MapView>
+            </View>
+            <View style={{position: 'absolute', right: 10}}>
+                <Button
+                    color="green"
+                    title="Refresh"
+                    // onPress={() => setRegion(location.latitude, location.longitude)}
+                ></Button>
+            </View>
+            <View style={styles.loc_container}>
+                <View style={styles.loc}>
+                    <Text style={styles.loc_name}>
+                        {isLoading ? "Loading" : JSON.stringify(places[1].name).slice(1,-1)}
+                    </Text>
+                    <Text>
+                        {isLoading ? "" : JSON.stringify(places[1].vicinity).slice(1,-1)}
+                    </Text>
+                </View>
+                <Button
+                    title="Change"
+                ></Button>
+            </View>
+            <View style= {{flex:8}}>
+
+            </View>
+        </View>
+        );
+    }
+    
+    const styles = StyleSheet.create({
+        screen: {
+            flex: 1,
+            padding: 10,
+        },
+      container: {
+        flex: 3,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height / 4,
+      },
+      loc_container: {
+          padding: 30,
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+      },
+      loc_name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+      }
+    });
--- app/src/styles/mainStyles.js
+++ app/src/styles/mainStyles.js
@@ -5,6 +5,7 @@ const mainStyles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
+        justifyContent: 'center'
     }
   });
 
