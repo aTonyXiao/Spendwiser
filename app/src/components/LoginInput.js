6c53925f44fbc3a2157fb023cd727873a0b3c242
NathanJWr <nathanielwright@protonmail.com>
2021-02-06 13:38:34 -0800

    Username and Password inputs are now function components

--- app/src/components/LoginInput.js
+++ app/src/components/LoginInput.js
@@ -3,57 +3,37 @@ import { TextInput, View, StyleSheet, Button } from 'react-native';
 import mainStyles from '../styles/mainStyles';
 
 const grayRGB = 'rgb(211, 211, 211)';
-export class UsernameInput extends React.Component {
-    state = {
-        email: ''
-    };
-
-    onChangeEmail = (val) => {
-        this.setState({
-            email: val
-        }, () => {
-            this.props.onChange(this.state.email)
-        });
+export const UsernameInput = props => {
+    async function onChangeEmail(val) {
+        props.onChange(val);
     }
 
-    render () {
-        return (
-            <TextInput
-                style={styles.input}
-                onChangeText={text => this.onChangeEmail(text)}
-                placeholder={'Username'}
-                placeholderTextColor={grayRGB}
-                textContentType={'emailAddress'}
-            />
-        );
-    }
+    return (
+        <TextInput
+            style={styles.input}
+            onChangeText={text => onChangeEmail(text)}
+            placeholder={'Username'}
+            placeholderTextColor={grayRGB}
+            textContentType={'emailAddress'}
+        />
+    );
 }
 
-export class PasswordInput extends React.Component {
-    state = {
-        password: ''
-    };
-
-    onChangePassword = (val) => {
-        this.setState({
-            password: val
-        }, () => {
-            this.props.onChange(this.state.password)
-        });
+export const PasswordInput = props => {
+    async function onChangePassword(val) {
+        props.onChange(val);
     }
 
-    render() {
-        return (
-            <TextInput
-                style={styles.input}
-                onChangeText={val => this.onChangePassword(val)}
-                placeholder={'Password'}
-                placeholderTextColor={grayRGB}
-                textContentType={'newPassword'}
-                secureTextEntry={true}
-            />
-        );
-    }
+    return (
+        <TextInput
+            style={styles.input}
+            onChangeText={val => onChangePassword(val)}
+            placeholder={'Password'}
+            placeholderTextColor={grayRGB}
+            textContentType={'newPassword'}
+            secureTextEntry={true}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
