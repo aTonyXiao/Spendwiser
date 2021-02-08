2420609faab6e9f6756e2f0bff84880867f8bf77
Wei Xuan (Nathan) Ng <wxng@ucdavis.edu>
2021-02-07 16:00:43 -0800

    edited list of stores to show up to 10 stores

--- app/src/components/MainScreen.js
+++ app/src/components/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Image, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Dimensions, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { TextInput } from 'react-native-gesture-handler';
@@ -35,14 +35,20 @@ export function MainScreen() {
             return;
         }
         let add_count = 0;
-        for (let i = 0; add_count < 5; i++) {
+        let fetch_result_len = Object.keys(fetch_result).length - 1;
+        for (let i = 0; add_count < 10; i++) {
+            if (i === fetch_result_len) {
+                console.log("got in here")
+                break;
+            }
             if (fetch_result[i].types.includes("locality")) {
                 continue;
             } else {
+                let storeType = JSON.stringify(fetch_result[i].types[0]).slice(1,-1).replace(/_/g, " ");
                 fetch_location.push({
                     name: JSON.stringify(fetch_result[i].name).slice(1,-1),
                     vicinity: JSON.stringify(fetch_result[i].vicinity).slice(1,-1),
-                    store_type: JSON.stringify(fetch_result[i].types[0]).slice(1,-1).replace(/_/g, " "), 
+                    store_type: storeType.charAt(0).toUpperCase() + storeType.slice(1), 
                 })
                 fetch_stores.push({
                     label: JSON.stringify(fetch_result[i].name).slice(1,-1),
@@ -88,72 +94,75 @@ export function MainScreen() {
     // console.log(text);
 
     return (
-        <View style={styles.screen}>
-            <View style={styles.map_container}>
-                <MapView style={styles.map}
-                    provider = "google"
-                    region = {region}
-                >
-                <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
-                </MapView>
-            </View>
-            <View style={{position: 'absolute', right: 10}}>
-                <Button
-                    color="green"
-                    title="Refresh"
-                    // onPress={() => setRegion(location.latitude, location.longitude)}
-                ></Button>
-            </View>
-            <View style={styles.loc}>
-                <RNPickerSelect
-                    placeholder={{}}
-                    items={storeArr}
-                    onValueChange={(value, key) => {setCurStore(value), setCurStoreKey(key)}}
-                    style={{...pickerSelectStyles,
-                        iconContainer: {
-                            top: 10,
-                            right: 12,
-                        },
-                }}
-                    value={curStore}
-                    useNativeAndroidPickerStyle={false}
-                    Icon={() => {
-                        return <Ionicons name="md-arrow-down" size={24} color="gray" />;
-                    }}
-                />
-                {/* <Text style={styles.loc_name}>
-                    {isLoading ? "Loading" : locations[0].name}
-                </Text> */}
-                <Text>
-                    {"Address: " + (isLoading ? "" : locations[curStoreKey].vicinity)}
-                </Text>
-                <Text>
-                    {"Category: " + (isLoading ? "" : locations[curStoreKey].store_type)}
-                </Text>
-            </View>
-            <View style= {{flex:1}}/>
-            <View style={styles.card_container}>
-                <Text style={{fontWeight: 'bold', fontSize: 24}}>Recommended Card</Text>
-                <Image style={styles.card}
-                    source={require('../../assets/sapphire_reserve_card.png')} />
-            <View style={styles.card_spending}>
-                    <TextInput
-                        style={styles.amount_field}
-                        onChangeText={text => setAmountSpent(text)}
-                        value={amountSpent}
-                        placeholder={"Amount Spent"}
-                        keyboardType='numeric'
-                    />
+        <SafeAreaView style={styles.screen}>
+            <ScrollView>
+                <View style={styles.map_container}>
+                    <MapView style={styles.map}
+                        provider = "google"
+                        region = {region}
+                    >
+                    <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+                    </MapView>
+                </View>
+                <View style={{position: 'absolute', right: 10}}>
                     <Button
-                        title="Update"
-                        // onPress={() => }
+                        color="green"
+                        title="Refresh"
+                        // onPress={() => setRegion(location.latitude, location.longitude)}
                     ></Button>
                 </View>
-            </View>
-            <View style= {{flex:2}}>
-
-            </View>
-        </View>
+                <View style={styles.loc}>
+                    <RNPickerSelect
+                        placeholder={{}}
+                        items={storeArr}
+                        onValueChange={(value, key) => {setCurStore(value), setCurStoreKey(key)}}
+                        style={{...pickerSelectStyles,
+                            iconContainer: {
+                                top: 10,
+                                right: 12,
+                            },
+                    }}
+                        value={curStore}
+                        useNativeAndroidPickerStyle={false}
+                        Icon={() => {
+                            return <Ionicons name="md-arrow-down" size={24} color="gray" />;
+                        }}
+                    />
+                    <Text>
+                        {"Address: " + (isLoading ? "" : locations[curStoreKey].vicinity)}
+                    </Text>
+                    <Text>
+                        {"Category: " + (isLoading ? "" : locations[curStoreKey].store_type)}
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => {console.log("store pressed")}}
+                    >
+                        <Text style={{color: 'blue'}}>Store not in list</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style= {{flex:1}}/>
+                <View style={styles.card_container}>
+                    <Text style={{fontWeight: 'bold', fontSize: 24}}>Recommended Card</Text>
+                    <Image style={styles.card}
+                        source={require('../../assets/sapphire_reserve_card.png')} />
+                <View style={styles.card_spending}>
+                        <TextInput
+                            style={styles.amount_field}
+                            onChangeText={text => setAmountSpent(text)}
+                            value={amountSpent}
+                            placeholder={"Amount Spent"}
+                            keyboardType='numeric'
+                        />
+                        <Button
+                            title="Update"
+                            // onPress={() => }
+                        ></Button>
+                    </View>
+                </View>
+                <View style= {{flex:2}}>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
         );
     }
     
