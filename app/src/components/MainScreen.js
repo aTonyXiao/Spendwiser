135cefde55e0eb3195d823a65a92afbee259fa5b
Wei Xuan (Nathan) Ng <wxng@ucdavis.edu>
2021-02-09 01:35:55 -0800

    add ability to manually add store

--- app/src/components/MainScreen.js
+++ app/src/components/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Image, Dimensions, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Dimensions, TouchableOpacity, ScrollView, SafeAreaView, Modal } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { TextInput } from 'react-native-gesture-handler';
@@ -20,11 +20,14 @@ export function MainScreen() {
     const [isLoading, setLoading] = useState(true);
     const [places, setPlaces] = useState([]);
     const [amountSpent, setAmountSpent] = useState("");
-    const [locations, setLocations] = useState([]);
     const [storeArr, setStoreArr] = useState([]);
     const [curStore, setCurStore] = useState(null);
     const [curStoreKey, setCurStoreKey] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [manualInput, setManualInput] = useState({store_name: "", vicinity: "", store_type: ""});
       
+    const handleManualInput = (val, inputStr) => setManualInput({...manualInput, [val]: inputStr});
+
     function getLocationFromAPI(json) {
         setPlaces(json.results);
         let fetch_result = json.results;
@@ -36,23 +39,19 @@ export function MainScreen() {
         }
         let add_count = 0;
         let fetch_result_len = Object.keys(fetch_result).length - 1;
-        for (let i = 0; add_count < 10; i++) {
+        for (let i = 0; i < fetch_result_len; i++) {
             if (i === fetch_result_len) {
-                console.log("got in here")
                 break;
             }
             if (fetch_result[i].types.includes("locality")) {
                 continue;
             } else {
                 let storeType = JSON.stringify(fetch_result[i].types[0]).slice(1,-1).replace(/_/g, " ");
-                fetch_location.push({
-                    name: JSON.stringify(fetch_result[i].name).slice(1,-1),
-                    vicinity: JSON.stringify(fetch_result[i].vicinity).slice(1,-1),
-                    store_type: storeType.charAt(0).toUpperCase() + storeType.slice(1), 
-                })
                 fetch_stores.push({
                     label: JSON.stringify(fetch_result[i].name).slice(1,-1),
                     value: JSON.stringify(fetch_result[i].name).slice(1,-1),
+                    vicinity: JSON.stringify(fetch_result[i].vicinity).slice(1,-1),
+                    store_type: storeType.charAt(0).toUpperCase() + storeType.slice(1), 
                     key: add_count,
                 })
                 if (add_count == 0) {
@@ -62,7 +61,6 @@ export function MainScreen() {
             add_count++;
             }
         }
-        setLocations(fetch_location);
         setStoreArr(fetch_stores);
     };
 
@@ -91,10 +89,80 @@ export function MainScreen() {
         })();
     }, []);
     
-    // console.log(text);
-
     return (
         <SafeAreaView style={styles.screen}>
+            <Modal
+                    animationType="slide"
+                    transparent={true}
+                    backdropOpacity = {0.3}
+                    visible={modalVisible}
+                    onShow={() => setManualInput({store_name: "", vicinity: "", store_type: ""})}
+                    onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    }}>
+                    <View style={styles.modalCenteredView}>
+                        <View style={styles.modalView}>
+                            <TextInput
+                                style={styles.modalTextInput}
+                                onChangeText={text => {
+                                    setManualInput((prevState) => {
+                                        return {...prevState, store_name: text};
+                                    })
+                                }}
+                                value={manualInput.store_name}
+                                placeholder={"Store Name (Optional)"}
+                            />
+                            <TextInput
+                                style={styles.modalTextInput}
+                                onChangeText={text => {
+                                    setManualInput((prevState) => {
+                                        return {...prevState, vicinity: text};
+                                    })
+                                }}
+                                value={manualInput.vicinity}
+                                placeholder={"Address (Optional)"}
+                            />
+                            <TextInput
+                                style={styles.modalTextInput}
+                                onChangeText={text => {
+                                    setManualInput((prevState) => {
+                                        return {...prevState, store_type: text};
+                                    })
+                                }}
+                                value={manualInput.store_type}
+                                placeholder={"Category (Required)"}
+                            />
+                            <View style={{flexDirection: "row", marginTop: 10, justifyContent: 'space-between', padding: 10}}>
+                                <Button
+                                    onPress={() => {
+                                        setModalVisible(!modalVisible);
+                                        if (manualInput.store_type.length != 0) {
+                                            let manualInputObj = {
+                                                label: manualInput.store_name.length === 0 ? "Manual Input" : manualInput.store_name,
+                                                value: manualInput.store_name.length === 0 ? "Manual Input" : manualInput.store_name,
+                                                vicinity: manualInput.store_name.vicinity === 0 ? "N/A" : manualInput.vicinity,
+                                                store_type: manualInput.store_type,
+                                                key: Object.keys(storeArr).length - 1,
+                                            }
+                                            console.log(manualInputObj)
+                                            setStoreArr(storeList => storeList.concat(manualInputObj));
+                                            setCurStore(manualInput.store_name.length === 0 ? "Manual Input" : manualInput.store_name);
+                                            setCurStoreKey(Object.keys(storeArr).length - 1);
+                                        }                                        
+                                    }}
+                                    title="Set"> </Button>
+                                <View style={{width:20}} />
+                                <Button
+                                    onPress={() => {
+                                        setModalVisible(!modalVisible);
+                                    }}
+                                    title="Close"> </Button>
+                            </View>
+                            
+                        </View>
+                    </View>
+                </Modal>
+
             <ScrollView>
                 <View style={styles.map_container}>
                     <MapView style={styles.map}
@@ -116,6 +184,7 @@ export function MainScreen() {
                         placeholder={{}}
                         items={storeArr}
                         onValueChange={(value, key) => {setCurStore(value), setCurStoreKey(key)}}
+                        pickerProps={{mode:'dropdown', itemStyle:{height: 100}}}
                         style={{...pickerSelectStyles,
                             iconContainer: {
                                 top: 10,
@@ -129,13 +198,13 @@ export function MainScreen() {
                         }}
                     />
                     <Text>
-                        {"Address: " + (isLoading ? "" : locations[curStoreKey].vicinity)}
+                        {"Address: " + (isLoading ? "" : storeArr[curStoreKey].vicinity)}
                     </Text>
                     <Text>
-                        {"Category: " + (isLoading ? "" : locations[curStoreKey].store_type)}
+                        {"Category: " + (isLoading ? "" : storeArr[curStoreKey].store_type)}
                     </Text>
                     <TouchableOpacity
-                        onPress={() => {console.log("store pressed")}}
+                        onPress={() => {setModalVisible(true)}}
                     >
                         <Text style={{color: 'blue'}}>Store not in list</Text>
                     </TouchableOpacity>
@@ -169,7 +238,6 @@ export function MainScreen() {
     const styles = StyleSheet.create({
         screen: {
             flex: 1,
-            padding: 10,
         },
       map_container: {
         flex: 3,
@@ -205,6 +273,28 @@ export function MainScreen() {
           borderWidth: 1,
           flex: 1
       },
+      modalCenteredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        marginTop: 22,
+        padding: 22,
+        backgroundColor: 'rgba(128, 128, 128, 0.5)'
+      },
+      modalView: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+      },
+      modalTextInput: {
+        height: 40,
+        marginTop: 10,
+        borderColor: 'gray',
+        borderWidth: 1,
+      }
     });
 
     const pickerSelectStyles = StyleSheet.create({
