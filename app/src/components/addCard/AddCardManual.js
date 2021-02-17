49924a3bdbdf16be9d9135963696890e5187691a
Nathaniel Kong <nskong@ucdavis.edu>
2021-02-16 17:03:05 -0800

    fixed add card manual to new database schema

--- app/src/components/addCard/AddCardManual.js
+++ app/src/components/addCard/AddCardManual.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { TextBox } from '../TextBox';
-import { appBackend } from '../../network/backend';
 import { user } from '../../network/user';
+import { cards } from '../../network/cards';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState } from 'react';
 
-export class AddCardManual extends React.Component {
-    constructor(props) { 
-        super(props);
+export function AddCardManual({navigation}) { 
+    const inputName = React.createRef();
+    const inputUrl = React.createRef();
+    const [reward, setReward] = useState('cashback');
+    // TODO need to be able to handle multiple rewards
 
-        this.inputName = React.createRef();
-        this.inputRewards = React.createRef(); // TODO this should be a list
-
-        this.navigation = props.navigation;
-    }
-
-    onPress = () => { 
+    addCard = () => { 
         var userId = user.getUserId();
 
-        var name = this.inputName.current.state.text;
-        var rewards = this.inputRewards.current.state.text;
+        var name = inputName.current.state.text;
+        var url = inputUrl.current.state.text;
 
-        appBackend.dbAdd("users." + userId + ".cards", {
-            name: name,
-            rewards: rewards,
-        }, (id) => { 
-            console.log(id);
-        })
+        var rewards = [];
 
-        this.navigation.navigate('Cards');
+        cards.addCard(name, rewards, url).then((cardId)=>{
+            user.saveCardToUser(userId, cardId, 0, null);
+            navigation.navigate('Cards');
+        });
     } 
 
-    render () {
-        return (
-            <View>
-                <TextBox ref={this.inputName} placeholder={'your credit card title here '}/>
-                <TextBox ref={this.inputRewards} placeholder={'your rewards here'}/>
-                <Button
-                    title='Add this card'
-                    onPress={this.onPress}
-                />
-            </View>
-        );
-    }
+    return (
+        <View>
+            <TextBox ref={inputName} placeholder={'your credit card title here '} />
+            <Text>Below is a dropdown for reward type... needs some styling</Text>
+            <DropDownPicker
+                items={[
+                    {label: 'cashback', value: '1', selected: true}, 
+                    {label: 'airplane miles', value: '2'},
+                ]}
+                placeholder={"Select an item"}
+                onChangeItem={item => setReward(item)}
+            />
+            <TextBox ref={inputUrl} placeholder={'url'} />
+            <Button
+                title='Add this card'
+                onPress={addCard}
+            />
+        </View>
+    );
 }
--- app/src/components/cards/Cards.js
+++ app/src/components/cards/Cards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Button, View, Text, Alert } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Button, View, Text } from 'react-native';
 import mainStyles from '../../styles/mainStyles';
 import { Card } from './Card';
 import { user } from '../../network/user';
@@ -11,8 +11,6 @@ const styles = StyleSheet.create({
     },
 });
 
-
-//TODO rename this to YourCards
 export function Cards({navigation}) { 
     const [cards, setCards] = useState([]);
     const userId = user.getUserId();
