37d00fa75f55d465b4621352d6c8b80e72a2934a
Nathaniel Kong <nskong@ucdavis.edu>
2021-02-19 11:29:22 -0800

    styling for login page

--- app/src/components/login/Login.js
+++ app/src/components/login/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import mainStyles from '../../styles/mainStyles';
-import {UsernameInput, PasswordInput} from './LoginInput';
-import { View, StyleSheet, Button, Alert } from 'react-native';
-import { appBackend } from '../../network/backend'
+import { UsernameInput, PasswordInput } from './LoginInput';
+import { View, StyleSheet, Button, Alert, TouchableOpacity, Text } from 'react-native';
+import { appBackend } from '../../network/backend';
+
+const styles = StyleSheet.create({
+    title : { 
+        fontSize: 40,
+        color: '#28b573',
+        position: 'absolute',
+        top: 70
+    },
+    loginWrapper : {
+        margin: 15,
+        height: 40,
+        width: '80%',
+        borderColor: '#87CEFA',
+        borderWidth: 1,
+        backgroundColor: '#87CEFA',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    loginButton : { 
+        color: 'white'
+    },
+    forgotPasswordButton : {
+        color: 'dodgerblue',
+        left: '10%'
+    },
+    line: {
+        width: '80%',
+        borderBottomColor: 'lightgray',
+        borderBottomWidth: 1,
+        margin: 15
+    }
+})
 
 
 export const Login = props => {
@@ -30,25 +63,25 @@ export const Login = props => {
 
     return (
         <View style={mainStyles.container}>
+            <Text style={styles.title}>SpendWiser</Text>
+
             <UsernameInput onChange={setUsername} />
             <PasswordInput onChange={setPassword} />
+            <TouchableOpacity 
+                style={styles.forgotPasswordButton}
+                onPress={() => props.navigation.navigate('PasswordReset')}>
+                <Text style={styles.forgotPasswordButton}>
+                    Forgot your password?
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.loginWrapper} onPress={signIn}>
+                <Text style={styles.loginButton}>Log In</Text>
+            </TouchableOpacity>
+
+            <View style={styles.line}/>
+
             <Button
-                title='Sign In'
-                onPress={signIn}
-            ></Button>
-            <Button
-                title='Reset Password'
-                onPress={() => {
-                    props.navigation.navigate('PasswordReset');
-                }}
-            ></Button>
-            <Button
-                title="Don't have an account?"
-                onPress={() => {
-                    props.navigation.navigate('CreateAccount')
-                }}
-            ></Button>
-            <Button
+                color={'dodgerblue'}
                 title="Sign in with Facebook"
                 onPress={() => {
                     let loginProviders = appBackend.getLoginProviders();
@@ -56,7 +89,8 @@ export const Login = props => {
                 }}
             ></Button>
             <Button
-            title="Sign in with Google"
+                color={'dodgerblue'}
+                title="Sign in with Google"
                 onPress={() => {
                     let loginProviders = appBackend.getLoginProviders();
                     loginProviders.google.login();
