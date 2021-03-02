102c0559e3d677a557f9e4d9aff968fb547bdcf8
NathanJWr <nathanielwright@protonmail.com>
2021-03-02 09:32:06 -0800

    Querying for permissions in settings

--- app/src/components/Permissions.js
+++ app/src/components/Permissions.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { View, Button, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Button, Text, Alert } from 'react-native';
 import * as Permissions from 'expo-permissions';
 
 export function AppPermissions() {
-    const [permission, askForPermission] = Permissions.usePermissions(Permissions.CAMERA, { ask: true });
-    if (!permission || permission.status !== 'granted') {
+    const [ loading, setLoading ] = useState(true);
+    const [ notificationStatus, setNotificationStatus ] = useState("undetermined");
+    const [ cameraStatus, setCameraStatus ] = useState("undetermined");
+
+    
+    let getPermissions = async () => {
+        const notifications = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        const camera = await Permissions.getAsync(Permissions.CAMERA);
+
+        console.log(notifications);
+        setNotificationStatus(notifications.status);
+        setCameraStatus(camera.status);
+        setLoading(false);
+    }
+    //const [permission, askForPermission] = Permissions.usePermissions(Permissions.CAMERA, { ask: false });
+
+
+    if (loading) {
+        getPermissions();
+        return(
+            <View>
+                <Text>loading</Text>
+            </View>
+        )
+    } else {
         return (
             <View>
-                <Text>Permission is not granted</Text>
-                <Button title="Grant permission" onPress={askForPermission} />
+                <Button
+                    title="Notifications"
+                    onPress={() => {
+                        if (notificationStatus === 'undetermined' ||
+                            notificationStatus === 'denied') {
+                            Alert.alert("Please enable notifications in settings");
+                        }
+                    }}
+                />
+                <Button
+                    title="Camera"
+                    onPress={() => {
+                        if (cameraStatus === 'undetermined' ||
+                            cameraStatus ===  'denied') {
+                            Alert.alert("Please enable camera in settings");
+                        }
+                    }}
+                />
             </View>
         );
     }
-
-    return (
-        <View>
-        </View>
-    );
+   
 }
