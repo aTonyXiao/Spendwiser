e67345d5ab9e51edbd597c52288a36825452ebd9
NathanJWr <nathanielwright@protonmail.com>
2021-03-02 10:01:09 -0800

    Checking/Asking for permissions in a better way

--- app/src/components/Permissions.js
+++ app/src/components/Permissions.js
@@ -3,53 +3,36 @@ import { View, Button, Text, Alert } from 'react-native';
 import * as Permissions from 'expo-permissions';
 
 export function AppPermissions() {
-    const [ loading, setLoading ] = useState(true);
-    const [ notificationStatus, setNotificationStatus ] = useState("undetermined");
-    const [ cameraStatus, setCameraStatus ] = useState("undetermined");
+    const [ notificationPermissions, askForNotificationPermission ] =
+          Permissions.usePermissions(Permissions.NOTIFICATIONS, {ask: false});
+    const [ cameraPermissions, askForCameraPermissions ] =
+          Permissions.usePermissions(Permissions.CAMERA, {ask: false});
 
+ 
+    return (
+        <View>
+            <Button
+                title="Notifications"
+                onPress={() => {
+                    console.log(notificationPermissions);
+                    if (!notificationPermissions.granted || notificationPermissions.status !== 'granted') {
+                        askForNotificationPermission();
+                    } else {
+                        Alert.alert("Notifications have already been allowed");
+                    }
+                }}
+            />
+            <Button
+                title="Camera"
+                onPress={() => {
+                    if (!cameraPermissions.granted || cameraPermissions.status !== 'granted') {
+                        askForCameraPermissions();
+                    } else {
+                        Alert.alert("Camera permissions have already been allowed");
+                    }
+                }}
+            />
+        </View>
+    );
     
-    let getPermissions = async () => {
-        const notifications = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-        const camera = await Permissions.getAsync(Permissions.CAMERA);
-
-        console.log(notifications);
-        setNotificationStatus(notifications.status);
-        setCameraStatus(camera.status);
-        setLoading(false);
-    }
-    //const [permission, askForPermission] = Permissions.usePermissions(Permissions.CAMERA, { ask: false });
-
-
-    if (loading) {
-        getPermissions();
-        return(
-            <View>
-                <Text>loading</Text>
-            </View>
-        )
-    } else {
-        return (
-            <View>
-                <Button
-                    title="Notifications"
-                    onPress={() => {
-                        if (notificationStatus === 'undetermined' ||
-                            notificationStatus === 'denied') {
-                            Alert.alert("Please enable notifications in settings");
-                        }
-                    }}
-                />
-                <Button
-                    title="Camera"
-                    onPress={() => {
-                        if (cameraStatus === 'undetermined' ||
-                            cameraStatus ===  'denied') {
-                            Alert.alert("Please enable camera in settings");
-                        }
-                    }}
-                />
-            </View>
-        );
-    }
-   
 }
