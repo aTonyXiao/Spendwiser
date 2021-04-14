1a5ef326cc6577cf36c2ce82207b244165f35a20
Troi-Ryan Stoeffler <troiryans@gmail.com>
2021-04-14 15:51:27 -0700

    http requests for modifying cards

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,44 @@
-import express from 'express';
+import express from "express";
 
 import cardModel from "./models/card.js";
 
 // initialize express
 const app = express();
+app.use(express.json());
 
-// set up the routes
-app.get("/set_data", async (req, res) => {
-    const testCard = new cardModel();
-    await testCard.save();
-    res.send("Success!");
+// get all cards
+app.get("/cards", async (req, res) => {
+    cardModel.find((err, data) => {
+        res.json(data);
+    });
 });
 
-app.get("/get_data", async (req, res) => {
-    const data = await cardModel.find();
-    res.json(data);
+// get a specific card by ID
+app.get("/cards/:cardId", async (req, res) => {
+    cardModel.findById(req.params.cardId, (err, data) => {
+        res.json(data);
+    });
+});
+
+// create card request
+app.post("/cards", async (req, res) => {
+    cardModel.create(req.body, (err, card) => {
+        res.send(err ? "error" : card._id);
+    });
+});
+
+// update card request
+app.put("/cards/:cardId", async (req, res) => {
+    cardModel.findByIdAndUpdate(req.params.cardId, req.body, (err, card) => {
+        res.send(err ? "error" : "success");
+    });
+});
+
+// delete card request
+app.delete("/cards/:cardId", async (req, res) => {
+    cardModel.findByIdAndDelete(req.params.cardId, (err, card) => {
+        res.send(err ? "error" : "success");
+    });
 });
 
 export default app;
