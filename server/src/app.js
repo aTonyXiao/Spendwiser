76e8cc8fe11204012e8757c8c12896406bc20f71
Troi-Ryan Stoeffler <troiryans@gmail.com>
2021-04-14 16:13:35 -0700

    add better error checking, and send status

--- server/src/app.js
+++ server/src/app.js
@@ -9,35 +9,38 @@ app.use(express.json());
 // get all cards
 app.get("/cards", async (req, res) => {
     cardModel.find((err, data) => {
-        res.json(data);
+        if (err || data == null) res.sendStatus(500);
+        else res.json(data);
     });
 });
 
 // get a specific card by ID
 app.get("/cards/:cardId", async (req, res) => {
     cardModel.findById(req.params.cardId, (err, data) => {
-        res.json(data);
+        if (err || data == null) res.sendStatus(404);
+        else res.json(data);
     });
 });
 
 // create card request
 app.post("/cards", async (req, res) => {
-    cardModel.create(req.body, (err, card) => {
-        res.send(err ? "error" : card._id);
+    cardModel.create(req.body, (err, data) => {
+        if (err || data == null) res.sendStatus(500);
+        else res.send(data._id);
     });
 });
 
 // update card request
 app.put("/cards/:cardId", async (req, res) => {
-    cardModel.findByIdAndUpdate(req.params.cardId, req.body, (err, card) => {
-        res.send(err ? "error" : "success");
+    cardModel.findByIdAndUpdate(req.params.cardId, req.body, (err, data) => {
+        res.sendStatus(err || data == null ? 500 : 200);
     });
 });
 
 // delete card request
 app.delete("/cards/:cardId", async (req, res) => {
-    cardModel.findByIdAndDelete(req.params.cardId, (err, card) => {
-        res.send(err ? "error" : "success");
+    cardModel.findByIdAndDelete(req.params.cardId, (err, data) => {
+        res.sendStatus(err || data == null ? 500 : 200);
     });
 });
 
