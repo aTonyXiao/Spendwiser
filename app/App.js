5aab12968d84b17cac295ff2252fba14d22bb17a
Your Name <nskong@ucdavis.edu>
2021-04-26 15:21:17 -0700

    disable swiping/animation betweeen screens

--- app/App.js
+++ app/App.js
@@ -36,7 +36,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{headerShown: false}}
+        screenOptions={
+          {
+            headerShown: false,
+            swipeEnabled: false,
+            animationEnabled: false
+          }
+        }
       >
         <Stack.Screen
           name="Home"
