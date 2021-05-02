c04f9b3d4b4c2910dff653e06fb1d3a1e12fd80e
Your Name <nskong@ucdavis.edu>
2021-05-01 21:29:26 -0700

    add tap anywhere to dismiss keyboard when it's open

--- app/src/components/login/LoginInput.js
+++ app/src/components/login/LoginInput.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    input: {
-        margin: 15,
-        height: 40,
-        width: '80%',
-        borderColor: '#F0F0F0',
-        borderWidth: 1,
-        backgroundColor: '#F0F0F0',
-        borderRadius: 5,
-        marginTop: 8,
-        marginBottom: 8 
-    },
-});
-const grayRGB = 'rgb(192, 192, 192)';
 
 export const UsernameInput = props => {
     return (
@@ -40,3 +26,18 @@ export const PasswordInput = props => {
         />
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        height: 40,
+        width: '80%',
+        borderColor: '#F0F0F0',
+        borderWidth: 1,
+        backgroundColor: '#F0F0F0',
+        borderRadius: 5,
+        marginTop: 8,
+        marginBottom: 8 
+    },
+});
+const grayRGB = 'rgb(192, 192, 192)';
