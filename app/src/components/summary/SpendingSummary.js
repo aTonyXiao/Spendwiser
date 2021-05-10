0655d2a0697f284ff479b4f1af006656b3f2788f
Nathaniel Wright <NathanielWright@protonmail.com>
2021-05-10 15:53:58 -0700

    only adding unique transactions to lists

--- app/src/components/summary/SpendingSummary.js
+++ app/src/components/summary/SpendingSummary.js
@@ -104,13 +104,20 @@ export function SpendingSummary({navigation}) {
         if (whichPeriod === 1 || whichPeriod === 0) {
             let endTimeFrame0 = new Date(newCompareTimeframe[0].getFullYear(), newCompareTimeframe[0].getMonth() + 1, 0);
             user.getTimeFrameTransactions(userId, newCompareTimeframe[0], endTimeFrame0, (data) => {
-                setCompareTransPeriod1(oldData => [...oldData, data]);
+                setCompareTransPeriod1(oldData => {
+                    console.log("Old data: ");
+                    console.log(oldData);
+
+                    console.log("New data: ");
+                    console.log([... new Set([...oldData, data])]);
+                    return [... new Set([...oldData, data])]
+                });
             });
         }
         if (whichPeriod === 2 || whichPeriod === 0) {
             let endTimeFrame1 = new Date(newCompareTimeframe[1].getFullYear(), newCompareTimeframe[1].getMonth() + 1, 0);
             user.getTimeFrameTransactions(userId, newCompareTimeframe[1], endTimeFrame1, (data) => {
-                setCompareTransPeriod2(oldData => [...oldData, data]);
+                setCompareTransPeriod2(oldData => {return [... new Set([...oldData, data])]});
             });
         }
     };
@@ -146,7 +153,7 @@ export function SpendingSummary({navigation}) {
         });
         let [startTimeFrame, endTimeFrame] = summaryHelper.getTimeFrame(curTimeframe);
         user.getTimeFrameTransactions(userId, startTimeFrame, endTimeFrame, (data) => {
-            setTransactions(oldData => [...oldData, data]);
+            setTransactions(oldData => [... new Set([...oldData, data])]);
         });
     }, [curTimeframe]);
 
