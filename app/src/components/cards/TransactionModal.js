319ad97834a4cae7c6650d15c2167a030ef2a776
NathanJWr <nathanielwright@protonmail.com>
2021-05-17 12:58:23 -0700

    if storeLocation does not exist, don't try to use it and notify user

--- app/src/components/cards/TransactionModal.js
+++ app/src/components/cards/TransactionModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, Dimensions, Alert } from 'react-native';
 import Modal from 'react-native-modal';
 import { Ionicons } from '@expo/vector-icons';
 import { user } from '../../network/user';
@@ -29,30 +29,39 @@ function TransactionModal({
     }
 
     addTransaction = () => {
-        const inputIsValid = isInputValid(transactionInput);
-        if (inputIsValid) { 
-            user.saveTransaction(
-                userId, 
-                cardId, 
-                {
-                    storeName: storeInformation["label"],
-                    address: storeInformation["vicinity"],
-                    storeType: storeInformation["storeType"]
-                },
-                transactionInput, 
-                (docId) => { 
-                    user.addTransactionId(userId, docId);
-                    setHasConstructed(false);
-                }
-            );
-    
-            setShowTransactionModal(false);
+        if (!storeInformation) {
+            Alert.alert("Store location not found!",
+                        "Please wait until we can find your location before you add a transaction",
+                        [
+                            {text: "Ok"}
+                        ],
+                        { cancelable: false });
         } else {
-            setDisplayErrorText(true);
+            const inputIsValid = isInputValid(transactionInput);
+            if (inputIsValid) { 
+                user.saveTransaction(
+                    userId, 
+                    cardId, 
+                    {
+                        storeName: storeInformation["label"],
+                        address: storeInformation["vicinity"],
+                        storeType: storeInformation["storeType"]
+                    },
+                    transactionInput, 
+                    (docId) => { 
+                        user.addTransactionId(userId, docId);
+                        setHasConstructed(false);
+                    }
+                );
+        
+                setShowTransactionModal(false);
+            } else {
+                setDisplayErrorText(true);
 
-            setTimeout(function () {
-                setDisplayErrorText(false);
-            }, 2500)
+                setTimeout(function () {
+                    setDisplayErrorText(false);
+                }, 2500)
+            }
         }
     }
 
