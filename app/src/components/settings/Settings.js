322d8189c8ca92d1ae732e572f3f527057e26acb
Troi-Ryan Stoeffler <troiryans@gmail.com>
2021-05-23 23:30:51 -0700

    rework settings layout

--- app/src/components/settings/Settings.js
+++ app/src/components/settings/Settings.js
@@ -17,167 +17,169 @@ let Settings = (props) => {
     const width = Dimensions.get('window').width;
 
     return(
-        <SafeAreaView style={styles.screen}>
-            <View style={{alignItems: 'center', justifyContent: 'center', marginTop: "15%"}}>
-                <Image source = {require("../../../assets/spendwiser_logo.png")}
-                    style = {{ 
-                        width: width * .8,  //its same to '20%' of device width
-                        aspectRatio: 5, // <-- this
-                        resizeMode: 'contain', //optional
-                    }}
-                />
-            </View>
-            <View style={styles.settingsContainer}>
-                <TouchableOpacity    
-                    onPress={() => {
-                        Alert.alert(
-                            'Tell A Friend About Us!',
-                            'Find a friend near you (use whatever transportation means are necessary) and tell them all about us. Thank you.',
-                            [
-                                { text: "OK" }
-                            ],
-                            { cancelable: false }
-                        );
-                    }}
-                    style={styles.rowContainerTop}
-                >
-                    <Text>Tell a Friend!</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            iconStyle={{margin: 0}}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => { props.navigation.navigate('Permissions') }}
-                    style={styles.rowContainer}
-                >
-                    <Text>Permissions</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => { props.navigation.navigate('PrivacyPolicy') }}
-                    style={styles.rowContainer}
-                >
-                    <Text>Privacy</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => props.navigation.navigate('Account')}
-                    style={styles.rowContainer}
-                >
-                    <Text>Account</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => { props.navigation.navigate('About') }}
-                    style={styles.rowContainer}
-                >
-                    <Text>About</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        appBackend.resetPassword(null, (message) => {
+        <SafeAreaView style={mainStyles.screen}>
+            <View style={mainStyles.bodyContainer}>
+                <View style={{alignItems: 'center', justifyContent: 'center', marginTop: "15%"}}>
+                    <Image source = {require("../../../assets/spendwiser_logo.png")}
+                        style = {{ 
+                            width: width * .8,  //its same to '20%' of device width
+                            aspectRatio: 5, // <-- this
+                            resizeMode: 'contain', //optional
+                        }}
+                    />
+                </View>
+                <View style={styles.settingsContainer}>
+                    <TouchableOpacity    
+                        onPress={() => {
                             Alert.alert(
-                                "",
-                                message,
+                                'Tell A Friend About Us!',
+                                'Find a friend near you (use whatever transportation means are necessary) and tell them all about us. Thank you.',
                                 [
-                                    { text: "OK", onPress: () => console.log("OK Pressed") }
+                                    { text: "OK" }
                                 ],
                                 { cancelable: false }
                             );
-                        });
-                    }}
-                    style={styles.rowContainer}
-                >
-                    <Text>Reset password</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        Alert.alert(
-                            'Are you sure you would like to log out?',
-                            '',
-                            [
-                                {text: 'NO', onPress: () => console.log(''), style: 'cancel'},
-                                {text: 'YES', onPress: () => {
-                                    appBackend.signOut();
-                                    props.navigation.navigate('Login');
-                                }}
-                            ]
-                        );
-                    }}
-                    style={styles.rowContainer}
-                >
-                    <Text>Logout</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        storage.clearLocalDB();
-                    }}
-                    style={styles.rowContainer}
-                >
-                    <Text>DEBUG delete local storage</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        storage.printLocalDB();
-                    }}
-                    style={styles.rowContainer}
-                >
-                    <Text>DEBUG print local storage</Text>
-                    <Ionicons
-                            name="chevron-forward-outline"
-                            color="gray"
-                            size={24}
-                            style={{marginRight: -8}}
-                    ></Ionicons>
-                </TouchableOpacity>
+                        }}
+                        style={styles.rowContainerTop}
+                    >
+                        <Text>Tell a Friend!</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                iconStyle={{margin: 0}}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => { props.navigation.navigate('Permissions') }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>Permissions</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => { props.navigation.navigate('PrivacyPolicy') }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>Privacy</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => props.navigation.navigate('Account')}
+                        style={styles.rowContainer}
+                    >
+                        <Text>Account</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { props.navigation.navigate('About') }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>About</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            appBackend.resetPassword(null, (message) => {
+                                Alert.alert(
+                                    "",
+                                    message,
+                                    [
+                                        { text: "OK", onPress: () => console.log("OK Pressed") }
+                                    ],
+                                    { cancelable: false }
+                                );
+                            });
+                        }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>Reset password</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            Alert.alert(
+                                'Are you sure you would like to log out?',
+                                '',
+                                [
+                                    {text: 'NO', onPress: () => console.log(''), style: 'cancel'},
+                                    {text: 'YES', onPress: () => {
+                                        appBackend.signOut();
+                                        props.navigation.navigate('Login');
+                                    }}
+                                ]
+                            );
+                        }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>Logout</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            storage.clearLocalDB();
+                        }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>DEBUG delete local storage</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            storage.printLocalDB();
+                        }}
+                        style={styles.rowContainer}
+                    >
+                        <Text>DEBUG print local storage</Text>
+                        <Ionicons
+                                name="chevron-forward-outline"
+                                color="gray"
+                                size={24}
+                                style={{marginRight: -8}}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.footerContainer}>
+            <View style={mainStyles.footerContainer}>
                 <Footer navigation={navigation} />
             </View>
         </SafeAreaView>
