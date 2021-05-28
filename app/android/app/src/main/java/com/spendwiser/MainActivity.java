a11761063ba8148dd4f95156b67ebc943ac5998e
Your Name <nskong@ucdavis.edu>
2021-05-28 15:17:00 -0700

    ejected app

--- app/android/app/src/main/java/com/spendwiser/MainActivity.java
+++ app/android/app/src/main/java/com/spendwiser/MainActivity.java
@@ -1,4 +1,6 @@
 package com.spendwiser;
+import android.content.res.Configuration;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -10,7 +12,18 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
+
 public class MainActivity extends ReactActivity {
+
+    // Added automatically by Expo Config
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        sendBroadcast(intent);
+    }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
