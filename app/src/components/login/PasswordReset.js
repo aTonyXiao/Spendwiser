6017391cd52bff1e23dae8737f8c900140e7ef15
Wei Xuan (Nathan) Ng <wxng@ucdavis.edu>
2021-06-02 17:32:19 -0700

    add logo to password reset ui, add back button, and dismiss keyboard functionality

--- app/src/components/login/PasswordReset.js
+++ app/src/components/login/PasswordReset.js
@@ -1,29 +1,61 @@
 import React from 'react';
-import { View, StyleSheet, Button, Alert } from 'react-native';
+import { View, Image, Button, Alert, Dimensions, SafeAreaView, StyleSheet } from 'react-native';
 import { UsernameInput } from './LoginInput';
-import mainStyles from '../../styles/mainStyles';
 import { appBackend } from '../../network/backend'
+import { DismissKeyboard } from '../util/DismissKeyboard';
+import { BackButtonHeader } from '../util/BackButtonHeader';
 
 export const PasswordReset = props => {
     const [username, setUsername] = React.useState('');
     return (
-        <View style={mainStyles.container}>
-            <UsernameInput onChange={setUsername} />
-            <Button
-                title="Reset Password"
-                onPress={() => {
-                    appBackend.resetPassword(username, (message) => {
-                        Alert.alert(
-                            "",
-                            message,
-                            [
-                                { text: "OK" }
-                            ],
-                            { cancelable: false }
-                        );
-                    });
-                }}
-            ></Button>
-        </View>
+         <DismissKeyboard>
+            <SafeAreaView style={styles.screen}>
+                <BackButtonHeader navigation={props.navigation}/>
+                <View style={{marginTop: "25%"}}>
+                    <Image source = {require("../../../assets/spendwiser_logo.png")}
+                        style = {{ 
+                            width: Dimensions.get('window').width * .8,  //its same to '20%' of device width
+                            aspectRatio: 5, // <-- this
+                            resizeMode: 'contain', //optional
+                        }}
+                    />
+                </View>
+                <View style={styles.registerContainer}>
+                    <UsernameInput onChange={setUsername} />
+                    <Button
+                        title="Reset Password"
+                        onPress={() => {
+                            appBackend.resetPassword(username, (message) => {
+                                Alert.alert(
+                                    "",
+                                    message,
+                                    [
+                                        { text: "OK" }
+                                    ],
+                                    { cancelable: false }
+                                );
+                            });
+                        }}
+                    ></Button>
+                </View>
+            </SafeAreaView>
+        </DismissKeyboard>
     );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: "100%"
+    },
+    registerContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        width: "100%",
+        marginVertical: "25%"
+    },
+});
